c34314caf161287b133e5c4178db204d49ba2fe1
sninyy <128959123+sninyoung@users.noreply.github.com>
2024-12-16 18:00:06 +0900

    test

--- test_241216_js_popup/js/tab.js
+++ test_241216_js_popup/js/tab.js
@@ -2,12 +2,9 @@ $(document).ready(function(){
 
     let tab_name //변수 생성
 
-    $('.news .tab_list ul li button').on('click', function(){
-        tab_name = $(this).parent().attr('data-panel') //: 자신의(this) 부모(parent)의 data-panel 값(attr)
+    $('.news .tab_list ul li').on('click', function(){
+        tab_name = $(this).find('button').text()
         console.log(tab_name)
-
-        $(this).attr('title', '선택됨')
-        $(this).html('<span>선택됨</span>')
     })
 
 })// $(document).ready
