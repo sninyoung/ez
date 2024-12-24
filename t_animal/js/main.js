34779b2c6e4d0356a8a511e2b44c8850481e77f0
sninyy <128959123+sninyoung@users.noreply.github.com>
2024-12-24 14:21:52 +0900

    동물보호센터 선생님 파일

--- t_animal/js/main.js
+++ t_animal/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+    /**************************** 변수 ************************************/
+    let device_status //브라우저가 pc인지 mobile상태인지
+    let window_w //브라우저의 넓이
+
+    /**************************** visual 팝업 (시작) ************************************/
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
         effect: "fade", /* fade 효과 */
         autoplay: {  /* 팝업 자동 실행 */
@@ -28,6 +33,35 @@ $(document).ready(function(){
         $(this).hide()//play버튼 숨김
         $('.visual .btn_wrap .stop').show()//stop버튼 나타남
     })
+    /**************************** visual 팝업 (종료) ************************************/
+
+    /**************************** pc인지 mobile 구분 (시작)  ***********************************/
+    function device_chk(){
+        window_w = $(window).width()
+        if(window_w > 640){
+            device_status = 'pc'
+        }else{
+            device_status = 'mobile'
+        }
+        console.log(device_status)
+    }
+    device_chk() //로딩 되었을때 1번 실행
+    $(window).resize(function(){//브라우저가 리사이즈될때 1번 실행
+        device_chk()
+    })
+
+    /**************************** pc인지 mobile 구분 (종료)  ***********************************/
+    /**************************** header 고정 (시작)  ***********************************
+     * 
+     * 언제 : 스크롤할때, header에 마우스를 올렸을때
+     * 뭘 : header에 fixed
+     * 사라질때 : 맨 위로 스크롤 되었을때, header에서 마우스를 아웃했을때
+     * 1. 스크롤 내린 상태에서 header에 마우스를 올렸다가 내림 (fixed 사라지면 X )
+    */
+
+
+
+    /**************************** header 고정 (종료)  ************************************/
     
 
 })//document.ready
