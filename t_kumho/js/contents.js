31dba13b3ac27bcff1e9031e1330756c79573334
sninyy <128959123+sninyoung@users.noreply.github.com>
2025-01-10 13:13:35 +0900

    연혁 history 코딩 끝, 찾아오시는 길 map.html 코딩 시작

--- t_kumho/js/contents.js
+++ t_kumho/js/contents.js
@@ -2,3 +2,10 @@
 * contents.js
 * 각각의 콘텐츠 페이지에서 작동될 스크립트 저장
 */
+console.log('열림')
+$(document).ready(function(){
+
+    if($('.sub_contents').hasClass('cnt_history')){
+        console.log('있어요')
+    }
+})//$(document).ready
