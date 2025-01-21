7f6000ed4f64c0bc49824519401b4db2868f9443
sninyy <128959123+sninyoung@users.noreply.github.com>
2025-01-21 14:11:07 +0900

    생명의 숲 선생님 버전

--- t_2501220_forest/js/main.js
+++ t_2501220_forest/js/main.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
     const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
-		navigation: true, /* 오른쪽에 각 페이지의 paging */
-		navigationPosition: 'right', /* 위치 */
-		navigationTooltips: ['첫번째', '두번째', '세번째', '네번째'], /* 툴팁 */
-		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
+		navigation: false, /* 오른쪽에 각 페이지의 paging */
+		navigationPosition: 'left', /* 위치 */
+		navigationTooltips: ['Main', '나무심기', '숲 활동', '활동이야기'], /* 툴팁 */
+		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */	
+		/* navigation은 글자를 클릭할 수가 없음 - 다른 기능을 사용 anchor, menu */
 		
-		lockAnchors: true,
-		anchors: ['link1', 'link2', 'link3', 'link4'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
+		lockAnchors: false, /* anchor를 사용하려면 해제 */
+		menu: '.quick_nav',
+		anchors: ['main', 'tree', 'introduce', 'story'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
 
-		autoScrolling:true, /* 한페이지씩 스크롤 */
+		autoScrolling: true, /* 한페이지씩 스크롤 */
 		scrollHorizontally: true,
-
 		verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
-		
 		scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
 		afterLoad: function(origin, destination, direction, trigger){
@@ -21,7 +21,33 @@ $(document).ready(function(){
 				console.log('3번째 슬라이드가 로딩 되었을때');
 			}
 		},
+		responsiveWidth: 1000, /* fullpage를 적용시키지 않을 모바일 사이즈 */
+		responsiveHeight: 700,
+	});//fullpage
 
-		responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+		autoplay: {  /* 팝업 자동 실행 */
+			delay: 2500,
+			disableOnInteraction: true,
+		},
+
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+			el: '.swiper-pagination', /* 해당 요소의 class명 */
+			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+			type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+			renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+				return '<span class="' + className + '">' + (index + 1) + "</span>";
+			},
+		},
+		
+
+		navigation: {  /* 이전, 다음 버튼 */
+			nextEl: '.swiper-button-next',  /* 다음 버튼의 클래스명 */
+			prevEl: '.swiper-button-prev',  
+		},
+
 	});
 })//document.ready
--- test_250120_forest/js/main.js
+++ test_250120_forest/js/main.js
@@ -33,6 +33,35 @@ $(document).ready(function(){
 
     responsiveWidth: 1000, /* fullpage를 적용시키지 않을 모바일 너비 사이즈 */
     responsiveHeight: 740, /* fullpage를 적용시키지 않을 모바일 높이 사이즈 */
-});
+});//fullpage
+
+
+
+const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+	autoplay: {  /* 팝업 자동 실행 */
+		delay: 2500,
+		disableOnInteraction: true,
+	},
+
+	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+		el: '.swiper-pagination', /* 해당 요소의 class명 */
+		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		//type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+		// renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+		//     return '<span class="' + className + '">' + (index + 1) + "</span>";
+		// },
+	},
+	
+
+	navigation: {  /* 이전, 다음 버튼 */
+		nextEl: '.swiper-button-next',  /* 다음 버튼의 클래스명 */
+		prevEl: '.swiper-button-prev',  
+	},
+
+});//Swiper
+
         
 })//$(document).ready  /**news의 swiper 종료 */
