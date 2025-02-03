a9024c1a7b8967a760ea5282f9804d09fffec91c
sninyy <128959123+sninyoung@users.noreply.github.com>
2025-02-03 18:00:42 +0900

    어셈블  product코딩 중..

--- portfolio_01_assemble/js/main.js
+++ portfolio_01_assemble/js/main.js
@@ -5,17 +5,18 @@
 
 $(document).ready(function(){
     
-    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싸는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
             delay: 5000,
             disableOnInteraction: true,
         },
-
         effect: "fade", /* fade 효과 */
-
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.visual .ctrl_wrap .next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .ctrl_wrap .prev',  
+        },
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
             el: '.visual .ctrl_wrap .paging', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
@@ -27,60 +28,75 @@ $(document).ready(function(){
 
     });
 
-    $('.visual .ctrl_wrap button.stop').on('click', function(){
-        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-        $(this).hide()
-        $('.visual .ctrl_wrap button.play').show()
-    })
-    $('.visual .ctrl_wrap button.play').on('click', function(){
-        visual_swiper.autoplay.start();  /* 재생 기능 */
-        $(this).hide()
-        $('.visual .ctrl_wrap button.stop').show()
-    })
 
-
-    /* biz에 마우스를 오버했을 때 (시작)
-    *
-    */
-
-    $('.biz .list ul li').on('mouseenter', function(){
-        $('.biz .list ul li').removeClass('active')
-        $(this).addClass('active')
-        $('.biz .list').addClass('over')
-    })
-    $('.biz .list').on('mouseleave', function(){
-        $('.biz .list ul li').removeClass('active')
-        $('.biz .list').removeClass('over')
-    })
-    /* biz에 마우스를 오버했을 때 (종료)*/
-
-
-
-/**news의 swiper (시작) */
-    const news_swiper = new Swiper('.news .list .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 24, /* 팝업과 팝업 사이 여백 */
+     /************************* product 팝업 (시작)  **************************/
+     const product_panel01_swiper = new Swiper('.product .panel01 .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
-            768: {    /* 768px 이상일때 적용 */
+            361: {    /* 361px이상 640px이하일때 적용 */
+                slidesPerView: 2,
+                spaceBetween: 24,
+            },
+            641: {    /* 641px이상 1000px이하일때 적용 */
+                slidesPerView: 3,
+                spaceBetween: 24,
+            },
+            1001: {    /* 1001px이상일때 적용 */
                 slidesPerView: 4,
                 spaceBetween: 24,
             },
         },
-        loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        scrollbar: {
-            el: ".news .list .ctrl_wrap .scroll",
-            hide: false, /*보이게 */
-            draggable: true,
-            dragSize: 120,
-          },
-
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.product .tab_content .panel01 .btn_wrap .next',
+            prevEl: '.product .tab_content .panel01 .btn_wrap .prev',
+        },
     });
-    /* news의 swiper (종료)*/
-
-    /*AOS 애니메이션 */
-    AOS.init({
-        offset: 150, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
-        duration: 500, // 애니메이션 효과가 작동되는 시간
-        easing: 'ease', // 가속도
+    const product_panel02_swiper = new Swiper('.product .panel02 .swiper', { /* 팝업을 감싸는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            641: {    /* 1300px 이상일때 적용 */
+                slidesPerView: 2,
+                spaceBetween: 24,
+            },
+            801: {    /* 1300px 이상일때 적용 */
+                slidesPerView: 3,
+                spaceBetween: 24,
+            },
+            1001: {    /* 1300px 이상일때 적용 */
+                slidesPerView: 4,
+                spaceBetween: 24,
+            },
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.product .tab_content .panel02 .btn_wrap .next',
+            prevEl: '.product .tab_content .panel02 .btn_wrap .prev',
+        },
     });
-})//$(document).ready  /**news의 swiper 종료 */
+
+    /************************* product 팝업 (끝)  **************************/
+    /************************* product 탭 기능 (시작)  *************************
+     * .product .tab_list ul li 버튼을 누른 후 하는 일
+     * li에 active 클래스 추가
+     * li button에 title="선택됨" 입력
+     * li에 data-tab의 값을 가져와서 .tab_content .tab_panel 중에서 data-tab이 같은 값인 요소 찾아서 active 클래스
+    */
+    $('.product .tab_list ul li').on('click', function(){
+        $('.product .tab_list ul li').removeClass('active')
+        $(this).addClass('active')
+        $('.product .tab_list ul li button').attr('title', '')
+        $(this).product('button').attr('title', '선택됨')
+        tab_name = $(this).attr('data-tab')
+        console.log(tab_name)
+        $('.product .tab_content .tab_panel').removeClass('active')
+        $('.product .tab_content').product('[data-tab="'+tab_name+'"]').addClass('active')
+    })
+    /*************************  product 탭 기능 (끝)  **************************/
+
+
+
+
+})//$(document).ready 
