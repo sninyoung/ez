2c669bf9c1944fd7bb783129396418b990a7c02a
sninyy <128959123+sninyoung@users.noreply.github.com>
2025-02-28 18:02:36 +0900

    footer까지 끝, 반응형 안함. 핵심가치 aos 애니메이션 하기 전

--- portfolio_02_greenpeace/js/main.js
+++ portfolio_02_greenpeace/js/main.js
@@ -31,17 +31,12 @@ $(document).ready(function(){
     * >>> 스크롤 1000일때 고정 시작
     *     스크롤 2000일때 고정 종료   ---- 스크롤 1000동안 사이즈가 리사이즈됨
     * >>> 리사이즈 시작 값은 75
-    *     리사이즈 종료 값은 100 --- 20만 변경됨...
-    * 
-    * >>> 예를들어 내가 현재 스크롤 값이 1500임 ... 그럼 현재 넓이가 얼마 ...
-    *     v_area_gap = 2000(v_end) - 1000(v_start) = 1000
-    *     v_scroll_per = 1500(scrolling) - 1000(v_start) = 500 / 1000(v_area_gap) = 0.5
-    *     v_resize_w = 100(v_resize_end) - 80(v_resize_start) = 20 * 0.5(v_scroll_per) = 10 + 80(v_resize_start) = 90
+    *     리사이즈 종료 값은 100 
    */
 
    let v_area_name =  $('.visual .video_area')
    let v_resize_name = $('.visual .video_area .video_wrap .video_inner')
-   let v_resize_start = 75 //리사이즈 시작값 동영상의 75%
+   let v_resize_start = 75 //리사이즈 시작값 =동영상의 75%
    let v_resize_end = 100 //리사이즈 종료값
    let v_resize_w // 리사이즈 될때 계산한 넓이값
    let v_area_gap // 리사이즈를 계산해야할 스크롤 구간값
@@ -84,11 +79,6 @@ $(document).ready(function(){
     /************************* visual 동영상 (끝)  **************************/
 
 
-    /************************* campaign 왼쪽 제목 고정  (시작)  *************************/
-    
-   
-    /************************* campaign 왼쪽 제목 고정 (끝)  *************************/
-
     /************************* campaign 아코디언 (시작)  *************************/
     gsap.registerPlugin(ScrollTrigger);
     const items = gsap.utils.toArray(".campaign .accordion");
@@ -96,13 +86,12 @@ $(document).ready(function(){
     items.forEach((item, i) => {
         const content = item.querySelector(".campaign .accordion .conts");
         const header = item.querySelector(".campaign .accordion .tit");
-        //console.log ('아코디언 js연결');
         gsap.to(content, {
             height: 0,
             ease: "none",
             scrollTrigger: {
                 trigger: item,
-                start: "top " + ((header.clientHeight * i)+150),
+                start: "top " + ((header.clientHeight * i)+40),
                 endTrigger: ".final",  // 고정요소 하단에 종료를 뜻하는 class
                 end: "top " + header.clientHeight * items.length,
                 pin: true,
@@ -139,10 +128,10 @@ $(document).ready(function(){
     /*************************project 스위퍼 (시작)  *************************/
     const support_swiper = new Swiper('.support .swiper', { /* 팝업을 감싸는 요소의 class명 */
         autoplay: {/* 팝업 자동 실행 */
-            delay: 5000,
+            delay: 4000,
             disableOnInteraction: false,
         },
-        speed: 5000,
+        speed: 4000,
         effect:'fade',
         loop: true, /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
         slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
@@ -151,26 +140,55 @@ $(document).ready(function(){
     /*************************project 스위퍼 (시작)  *************************/
 
     /*************************news 스위퍼 (시작)  *************************/
-    const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싸는 요소의 class명 */
-        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 12, /* 팝업과 팝업 사이 여백 */
+    const news_swiper = new Swiper('.news .swiper', { 
+    
+        autoplay: {/* 팝업 자동 실행 */
+        delay: 2000,
+        disableOnInteraction: false,
+        },
+        speed: 500,
+        loop: true, 
+        slidesPerView: 1,
+        slidesPerView: 2,
+        spaceBetween: 0,
         breakpoints: {
             381: {    /* 381px이상 768px이하일때 적용 */
                 slidesPerView: 2,
-                spaceBetween: 16,
+                spaceBetween: 0,
             },
             769: {    /* 769px이상 1024px이하일때 적용 */
                 slidesPerView: 3,
-                spaceBetween: 16,
+                spaceBetween: 0,
             },
             1025: {    /* 1025px이상일때 적용 */
-                slidesPerView: 3,
-                spaceBetween: 24,
+                slidesPerView: 4,
+                spaceBetween: 0,
             },
         },
-        loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
     });
     /*************************news 스위퍼 (끝)  **************************/
+
+    /*************************footer intl_site 열기 open(시작)  *************************
+     * footer .intl_site button  클릭했을때
+     * footer .intl_site 에 open 클래스 추가
+     * button에 title의 문구 변경
+     * footer .intl_site .list 열고 닫기 slideUp slideDown
+     * >> 현재 버튼이 열려있는지 닫혀 있는지 구분 >> open클래스 존재 유무로
+    */
+      $('footer .intl_site button').on('click', function(){
+        if($(this).parent().hasClass('open') == true){//open클래스가 있을때 (열렸을때 >> 닫는기능)
+            //console.log('open클래스 있음')
+            $(this).parent().removeClass('open')
+            $(this).next().slideUp()
+            $(this).attr('title', '열기버튼')
+        }else{ //open클래스가 없을때 (닫혔을때 >>> 여는기능)
+            //console.log('open클래스 없음')
+            $(this).parent().addClass('open')
+            $(this).next().slideDown()
+            $(this).attr('title', '닫기버튼')
+        }
+    })
+    /*************************footer intl_site 열기 open (끝)  **************************/
 
 
 
